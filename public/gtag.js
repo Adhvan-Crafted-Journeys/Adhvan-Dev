e28be9092085cdaa2e110fa04e7d8262897362f1
Benatic98 <benatic98@gmail.com>
2025-12-16 11:37:58 +0530

    SEO Check

---
+++ public/gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-69N551532P"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-69N551532P');
+</script>
